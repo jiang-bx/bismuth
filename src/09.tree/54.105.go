1e6e2520fa595b67264f02e6b603eb7af60528b8
jiangbx <jbxshare@163.com>
2024-06-13 12:07:17 +0800

    feat: leetcode 105

--- src/09.tree/54.105.go
+++ src/09.tree/54.105.go
@@ -1,5 +1,25 @@
 package tree
 
 func BuildTree(preorder []int, inorder []int) *TreeNode {
-	return nil
+	var dfs func(cur, left, right int) *TreeNode
+	m := map[int]int{}
+	for i, v := range inorder {
+		m[v] = i
+	}
+
+	dfs = func(cur, left, right int) *TreeNode {
+		if left > right {
+			return nil
+		}
+
+		node := &TreeNode{Val: preorder[cur]}
+		i := m[preorder[cur]]
+
+		node.Left = dfs(cur+1, left, i-1)
+		node.Right = dfs(cur+(i-left)+1, i+1, right)
+
+		return node
+	}
+
+	return dfs(0, 0, len(inorder)-1)
 }
--- src/09.tree/54.105.ts
+++ src/09.tree/54.105.ts
@@ -4,5 +4,31 @@ export function buildTree(
     preorder: number[],
     inorder: number[]
 ): TreeNode | null {
-    return null;
+    /**
+     * 先序遍历, [根节点 | 左子树 | 右子树]
+     * 中序遍历, [左子树 | 根节点 | 右子树]
+     */
+
+    const m = new Map<number, number>();
+    inorder.forEach((item, i) => {
+        m.set(item, i);
+    });
+
+    const dfs = (cur: number, left: number, right: number): TreeNode | null => {
+        if (left > right) {
+            return null;
+        }
+
+        const node = new TreeNode(preorder[cur]);
+
+        // i 是 中序遍历中的 的父节点 index 值
+        const i = m.get(preorder[cur]);
+
+        node.left = dfs(cur + 1, left, i - 1);
+        node.right = dfs(cur + (i - left) + 1, i + 1, right);
+
+        return node;
+    };
+
+    return dfs(0, 0, inorder.length - 1);
 }
